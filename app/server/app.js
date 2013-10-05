94bf21de9e8984090ce66c4d1026a4fa16358b93
StuartCoope <stuartcoope@gmail.com>
2013-10-05 16:14:32 +0100

    Store sessions in database. Support for roles

--- app/server/app.js
+++ app/server/app.js
@@ -3,16 +3,17 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-
-var app = express();
+var express = require('express'),
+	routes = require('./routes'),
+	userRoute = require('./routes/userRoute'),
+	http = require('http'),
+	path = require('path'),
+	app = express(),
+    MongoStore = require('express-session-mongo')
+    ;
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', __dirname + '/views');
 
 app.set('view engine', 'jade');
@@ -20,8 +21,15 @@ app.set('view options', {layout: true});
 
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.session({ 
+	store: new MongoStore(),
+	secret: 'securedsession'
+}));
+
+
 app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/app/public'));
 app.use(express.static(path.join(__dirname, '../public')));
@@ -31,10 +39,32 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//middleware function that checks if we are logged in
+var enforceRole = function(role){
+	return function(req, res, next){
+		if(req.session.roles.indexOf(role) !== -1){
+			next();
+		}else{
+			res.send(401);
+		}
+	}
+}
+
+
 app.get('/', routes.index);
-app.get('/users', user.list);
-app.get('/users/:id', user.findById);
+app.get('/users', enforceRole("admin"), userRoute.list);
+app.get('/users/:id', enforceRole("admin"), userRoute.findById);
+app.get('/login/:username/:password', userRoute.login);
+
+
+// route to test if the user is logged in or not 
+app.get('/loggedin', function(req, res) { 
+	res.send(req.session["loggedIn"] ? req.session["username"] : '0');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -4,5 +4,6 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	res.jsonp(req.session);
+  	//res.render('index', { title: 'Express' });
 };
