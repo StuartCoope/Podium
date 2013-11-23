0b3c58131177b83c09fc34147646174f97a03b24
Stuart <me@stu.io>
2013-11-23 14:05:06 +0000

    Test commit

--- app/public/tournaments/TournamentService.js
+++ app/public/tournaments/TournamentService.js
@@ -4,8 +4,8 @@
 angular.module("tournament.service", ["ngResource"], function($provide){
 	$provide.factory("TournamentService", function($resource){
 		
-		
-		
+
+
 		return $resource("api/podium/tournaments", {}, {
 			list: {method:'GET'}
 		});
